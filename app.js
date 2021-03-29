24b4de969b6941ec7c54b849b01c27788bc6f667
katungi <dankatdennis@gmail.com>
2021-03-29 11:37:44 +0300

    testing routes

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello APi');
+require('dotenv/config');
+
+// middleware
+
+app.use(express.json());
+
+const api = process.env.API_URL;
+
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: 'hair dresser',
+    image: 'some_url',
+  };
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  // res.send(product);
 });
 
 app.listen(3000, () => {
+  console.log(api);
   console.log(`running at 3000`);
 });
