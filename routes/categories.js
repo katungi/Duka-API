b5bfd2faa4913c45466e27aa046262d01c5186ee
katungi <dankatdennis@gmail.com>
2021-03-30 07:04:21 +0300

    added category

--- routes/categories.js
+++ routes/categories.js
@@ -11,4 +11,36 @@ router.get(`/`, async (req, res) => {
   res.send(categoryList);
 });
 
+router.post('/', async (req, res) => {
+  let category = new Category({
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color,
+  });
+
+  category = await category.save();
+
+  if (!category) return res.status(404).send('the category cannot be created!');
+
+  res.send(category);
+});
+
+router.delete('/:id', (req, res) => {
+  let id = req.params.id;
+  Category.findByIdAndRemove(id)
+    .then((category) => {
+      if (category) {
+        return res
+          .status(200)
+          .json({ success: true, message: 'the category is deleted!' });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: 'Category not found' });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
 module.exports = router;
