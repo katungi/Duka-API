0098ae2521583bb9949b3f601413429483e49e5b
katungi <dankatdennis@gmail.com>
2021-03-30 08:00:04 +0300

    added callback on update 🌮

--- routes/categories.js
+++ routes/categories.js
@@ -8,7 +8,36 @@ router.get('/', async (req, res) => {
   if (!categoryList) {
     res.status(500).json({ success: false });
   }
-  res.send(categoryList);
+  res.status(200).send(categoryList);
+});
+
+router.put('/:id', async (req, res) => {
+  let id = req.params.id;
+  const category = await Category.findByIdAndUpdate(
+    id,
+    {
+      name: req.body.name,
+      icon: req.body.icon,
+      color: req.body.color,
+    },
+    { new: true }
+  );
+
+  if (!category) return res.status(404).send('the category cannot be created!');
+
+  res.send(category);
+});
+
+router.get('/:id', async (req, res) => {
+  let id = req.params.id;
+  const category = await Category.findById(id);
+
+  if (!category)
+    return res
+      .status(500)
+      .json({ message: 'The Category with the given ID was removed' });
+
+  res.status(200).send(category);
 });
 
 router.post('/', async (req, res) => {
