c801993c5f115624c9ed6f5e8443ddcd6a03570b
katungi <dankatdennis@gmail.com>
2021-04-16 06:19:42 +0300

    added delete order functionality

--- routes/orders.js
+++ routes/orders.js
@@ -33,7 +33,7 @@ router.get(`/:id`, async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // only update order status
-  
+
   let id = req.params.id;
   const order = await Category.findByIdAndUpdate(
     id,
@@ -81,6 +81,25 @@ router.post('/', async (req, res) => {
   if (!order) return res.status(404).send('the order cannot be created!');
 
   res.send(order);
+});
+
+router.delete('/:id', (req, res) => {
+  let id = req.params.id;
+  Order.findByIdAndRemove(id)
+    .then((order) => {
+      if (order) {
+        return res
+          .status(200)
+          .json({ success: true, message: 'the order is deleted!' });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: 'order not found' });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
 });
 
 module.exports = router;
