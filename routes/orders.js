5b8d1722622b99f6afa31d70125d587312840aa6
katungi <dankatdennis@gmail.com>
2021-04-16 06:01:41 +0300

    resolved order items promises

--- routes/orders.js
+++ routes/orders.js
@@ -14,18 +14,21 @@ router.get(`/`, async (req, res) => {
 
 router.post('/', async (req, res) => {
   // you have to populate order-items first as an array
-  const orderItemsIds = req.body.orderItems.map(async orderItem=>{
-    let newOrderItem = new OrderItem({
-      quantity: orderItem.quantity,
-      product: orderItem.product
-    });
+  const orderItemsIds = Promise.all(
+    req.body.orderItems.map(async (orderItem) => {
+      let newOrderItem = new OrderItem({
+        quantity: orderItem.quantity,
+        product: orderItem.product,
+      });
 
-    newOrderItem = await newOrderItem.save();
-    return newOrderItem._id;
-  })
+      newOrderItem = await newOrderItem.save();
+      return newOrderItem._id;
+    })
+  );
 
+  const orderItemIdsResolved = await orderItemsIds;
   let order = new Order({
-    orderItems: orderItemsIds,
+    orderItems: orderItemIdsResolved,
     shippingAddress1: req.body.shippingAddress1,
     shippingAddress2: req.body.shippingAddress2,
     city: req.body.city,
