82b00b6cc4af7c2a522041c67af8c8e94f1607ad
katungi <dankatdennis@gmail.com>
2021-04-16 05:58:34 +0300

    created nested post

--- models/order.js
+++ models/order.js
@@ -8,6 +8,54 @@ const orderSchema = mongoose.Schema({
       required: true,
     },
   ],
+  shippingAddress1: {
+    type: String,
+    required: true,
+  },
+  shippingAddress2: {
+    type: String,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: 'Pending',
+  },
+  totalPrice: {
+    type: Number,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  dateOrdered: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+
+orderSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+
+orderSchema.set('toJSON', {
+  virtuals: true,
 });
 
 exports.Order = mongoose.model('Order', orderSchema);
--- routes/orders.js
+++ routes/orders.js
@@ -1,5 +1,6 @@
 const { Order } = require('../models/order');
 const express = require('express');
+const { OrderItem } = require('../models/order-item');
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
@@ -9,6 +10,38 @@ router.get(`/`, async (req, res) => {
     res.status(500).json({ success: false });
   }
   res.send(orderList);
+});
+
+router.post('/', async (req, res) => {
+  // you have to populate order-items first as an array
+  const orderItemsIds = req.body.orderItems.map(async orderItem=>{
+    let newOrderItem = new OrderItem({
+      quantity: orderItem.quantity,
+      product: orderItem.product
+    });
+
+    newOrderItem = await newOrderItem.save();
+    return newOrderItem._id;
+  })
+
+  let order = new Order({
+    orderItems: orderItemsIds,
+    shippingAddress1: req.body.shippingAddress1,
+    shippingAddress2: req.body.shippingAddress2,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    status: req.body.status,
+    totalPrice: req.body.totalPrice,
+    user: req.body.user,
+  });
+
+  order = await order.save();
+
+  if (!order) return res.status(404).send('the order cannot be created!');
+
+  res.send(order);
 });
 
 module.exports = router;
