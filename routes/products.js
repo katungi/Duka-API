5f9be6273de9443f1fcea6fb0743a1d04a1f29ab
Daniel Dennis <dankatdennis@gmail.com>
2021-05-29 06:49:44 +0300

    - validate if file type is correct

--- routes/products.js
+++ routes/products.js
@@ -5,12 +5,24 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 const router = express.Router();
 
+const FILE_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpeg',
+};
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/public/uploads');
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error('Invalid Image Type');
+    if (isValid) {
+      uploadError = null;
+    }
+    cb(uploadError, '/public/uploads');
   },
   filename: function (req, file, cb) {
     const filename = file.originalname.split(' ').join('-');
+    const extension = FILE_TYPE_MAP[file.mimetype];
     cb(null, `${filename}-${Date.now()}.${extension}`);
   },
 });
